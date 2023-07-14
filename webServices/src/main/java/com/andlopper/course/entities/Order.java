9da973920015fa12df94649909dabb4fe2bf1c68
André Pereira <andrelopespereira@hotmail.com>
2023-07-14 10:33:35 -0300

    Enum adicionado

--- webServices/src/main/java/com/andlopper/course/entities/Order.java
+++ webServices/src/main/java/com/andlopper/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.andlopper.course.entities;
 
+import com.andlopper.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -16,6 +17,8 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT") //Mostrar no formato ISO
     private Instant moment;
+
+    private Integer orderStatus;
     @ManyToOne //Chave estrangeira, indica que é muitos para um
     @JoinColumn(name = "client_id") //Nome da chave
     private User client;
@@ -23,9 +26,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -47,6 +51,15 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null)
+        this.orderStatus = orderStatus.getCode();
     }
 
     public void setClient(User client) {
