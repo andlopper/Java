48fa5e6e26314a38887f4d55d5cc58c3b584627b
André Pereira <andrelopespereira@hotmail.com>
2023-07-17 15:03:06 -0300

    Métodos subTotal e Total

--- webServices/src/main/java/com/andlopper/course/entities/Order.java
+++ webServices/src/main/java/com/andlopper/course/entities/Order.java
@@ -86,6 +86,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal(){
+        Double sum = 0.0;
+        for (OrderItem oi : items) {
+            sum += oi.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
