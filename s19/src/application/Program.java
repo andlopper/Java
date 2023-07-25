c5eb02fc777f0b0a77e6c85b4974753bdcede464
unknown <andrelopespereira@hotmail.com>
2023-07-25 20:53:55 -0300

    Programa código estudantes

--- s19/src/application/Program.java
+++ s19/src/application/Program.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Program {
@@ -13,6 +14,7 @@ public class Program {
         String path = "in.txt";
         Set<AccessLog> aL = new HashSet<>();
 
+        //Programa de log de acesso
         try (BufferedReader br = new BufferedReader(new FileReader(path))){
             String line = br.readLine();
             while (line != null){
@@ -25,5 +27,27 @@ public class Program {
         catch (IOException e){
             System.out.println(e.getMessage());
         }
+
+        //Programa de código de estudantes
+        Set<Integer> students = new HashSet<>();
+        Scanner sc = new Scanner(System.in);
+        int i, j, n, m;
+
+        System.out.printf("How many courses? ");
+        n = sc.nextInt();
+        for (i = 1; i <= n; i++) {
+            System.out.printf("How many students for course %d? ", i);
+            m = sc.nextInt();
+            for (j = 0; j < m; j++) {
+                System.out.printf("Student code: ");
+                students.add(sc.nextInt());
+            }
+        }
+        System.out.printf("Total students: " + students.size());
+
+
+
+
+        sc.close();
     }
 }
